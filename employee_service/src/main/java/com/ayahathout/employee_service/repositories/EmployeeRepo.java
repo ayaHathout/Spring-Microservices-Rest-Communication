2d5028f3059587dfad841b64c9bd19e5c4fe3b47
Aya <ayahathout1@gmail.com>
2026-02-03 01:27:11 +0200

    solve some issues in employee_service

--- employee_service/src/main/java/com/ayahathout/employee_service/repositories/EmployeeRepo.java
+++ employee_service/src/main/java/com/ayahathout/employee_service/repositories/EmployeeRepo.java
@@ -2,8 +2,6 @@ package com.ayahathout.employee_service.repositories;
 
 import com.ayahathout.employee_service.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 }
